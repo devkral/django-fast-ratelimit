c4ae71582dedaad2bb7fa254055c034994637e7e
alex <devkral@web.de>
2023-12-27 07:23:45 +0100

    fix settings DB path (and modernize), update deps

--- test_settings.py
+++ test_settings.py
@@ -1,38 +1,32 @@
+from pathlib import Path
 
-import os
-
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-)
-
+BASE_DIR = Path(__file__).parent.absolute()
 
 SECRET_KEY = "FOOBAR"
 
 MIDDLEWARE = []
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
 ]
 
-RATELIMIT_DEFAULT_CACHE = 'default'
+RATELIMIT_DEFAULT_CACHE = "default"
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'cache-ratelimits',
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "cache-ratelimits",
     },
-    'db': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'cache-ratelimits',
+    "db": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "cache-ratelimits",
     },
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
 }
